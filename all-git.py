f986f01b6a52e3df4a1cd143ae7622438695e2d0
Thomas Desveaux <desveaux.thomas@gmail.com>
2026-02-28 23:35:48 +0100

    all-git: make async

--- all-git.py
+++ all-git.py
@@ -1,16 +1,39 @@
 #!/usr/bin/python3
+from __future__ import annotations
 
+import asyncio
 import datetime
-import sys
-import click
-from pathlib import Path
+import shutil
 import subprocess
+import sys
+from collections.abc import Callable, Coroutine, Iterator
+from functools import wraps
+from pathlib import Path
+from typing import TYPE_CHECKING, Any, ParamSpec, TypeVar
+
+import click
+
+if TYPE_CHECKING:
+    _T = TypeVar("_T")
+    _P = ParamSpec("_P")
+
+
+def sync(func: Callable[_P, Coroutine[Any, Any, _T]]) -> Callable[_P, _T]:
+    """Decorator that wraps coroutine with asyncio.run."""
+
+    @wraps(func)
+    def wrapper(*args: _P.args, **kwargs: _P.kwargs) -> _T:
+        return asyncio.run(func(*args, **kwargs))
+
+    return wrapper
+
 
 @click.group()
-def cli():
+def cli() -> None:
     pass
 
-def enumerate_git():
+
+def enumerate_git() -> Iterator[Path]:
     cwd = Path()
     for p in cwd.rglob(".git"):
         if p.is_dir():
@@ -19,43 +42,56 @@ def enumerate_git():
             else:
                 yield p.parent.relative_to(cwd)
 
-@cli.command()
-def fetch():
-    for repo in enumerate_git():
-        display_name = f" {repo} "
-        display_len = max(len(display_name) + 4, 60)
-        print(f"\r{display_name:=^{display_len}}", end='', flush=True)
-        fetch_cmd = ["git", "fetch", "--all", "--prune", "--tags"]
-        process = subprocess.run(
-            fetch_cmd,
-            cwd=repo, text=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-        )
-        # retry once
-        if process.returncode:
-            process = subprocess.run(
-                fetch_cmd,
-                cwd=repo, text=True,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-            )
-        if process.returncode:
-            print(
-                f"command {fetch_cmd} in {repo} failed with code {process.returncode}.\n"
-                f"stdout: {process.stdout}\n"
-                f"stderr: {process.stderr}\n"
-                )
-        if any(
-            e.split()[0] != "Fetching"
-            for e
-            in process.stderr.splitlines(keepends=False)
-        ):
-            print()
-            print(process.stderr)
-            print(end='', flush=True)
 
-    print('\r' + ' ' * 60, flush=True, end='\r')
+@cli.command()
+@sync
+async def fetch() -> None:
+    git_exe = shutil.which("git")
+    if git_exe is None:
+        raise RuntimeError("Failed to find git")
+    fetch_cmd = ["fetch", "--all", "--prune", "--tags"]
+
+    async def _fetch(repo: Path) -> tuple[int | None, bytes, bytes]:
+        process = await asyncio.subprocess.create_subprocess_exec(
+            git_exe,
+            *fetch_cmd,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+            cwd=repo,
+        )
+        stdout, stderr = await process.communicate()
+        return process.returncode, stdout, stderr
+
+    async def _task(repo: Path) -> tuple[int | None, bytes, bytes]:
+        returncode, stdout, stderr = await _fetch(repo)
+        if returncode != 0:
+            returncode, stdout, stderr = await _fetch(repo)
+        return returncode, stdout, stderr
+
+    tasks = [
+        (repo, asyncio.create_task(_task(repo), name=f"fetch {repo}"))
+        for repo in enumerate_git()
+    ]
+
+    for idx, (repo, task) in enumerate(tasks):
+        display_name = f" {repo} ({idx+1}/{len(tasks)}) "
+        display_len = max(len(display_name) + 4, 60)
+        print(f"\r{display_name:=^{display_len}}", end="", flush=True)
+
+        returncode, stdout, stderr_b = await task
+        stderr = stderr_b.decode()
+        if returncode:
+            print(
+                f"command {git_exe} {fetch_cmd} in {repo} failed with code {returncode}.\n"
+                f"stdout: {stdout.decode()}\n"
+                f"stderr: {stderr}\n"
+            )
+        if any(e.split()[0] != "Fetching" for e in stderr.splitlines(keepends=False)):
+            print()
+            print(stderr)
+            print(end="", flush=True)
+
+    print("\r" + " " * 60, flush=True, end="\r")
 
 
 def print_git_logs(
